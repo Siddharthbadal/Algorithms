32e2fdea672ec49ccd005808202131a2bac4b3a6
Siddharth badal <siddharthabadal.2015@gmail.com>
2020-05-19 02:11:57 +0530

    Problem 1: Multiples of 3 and 5
    The solution to problem 1 in Project Euler.

---
+++ multiples.py
@@ -0,0 +1,11 @@
+def natural_numbers(num):
+    total = 0
+    for i in range(num):
+        if (i % 3) == 0 or (i % 5) == 0:
+            total = total + i
+    print(total)
+
+
+natural_numbers(1000)
+
+
